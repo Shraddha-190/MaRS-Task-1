83a3e123a747c3a8583f3f033e17af22ce6c1b06
Shraddha-190 <ec25i2022@iiitdm.ac.in>
2026-04-06 21:53:40 +0530

    Update code.ino

--- mini project/code.ino
+++ mini project/code.ino
@@ -1,1 +1,28 @@
+int gasSensor = A0;
+int ledPin = 7;
+int buzzer = 8;
+
+int threshold = 400;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int gasValue = analogRead(gasSensor);
+
+  Serial.println(gasValue);
+
+  if (gasValue > threshold) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(buzzer, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(buzzer, LOW);
+  }
+
+  delay(500);
+}
 
