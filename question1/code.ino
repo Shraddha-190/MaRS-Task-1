ae719752d47ad0866d49764bd69cc8338bf5bbea
Shraddha-190 <ec25i2022@iiitdm.ac.in>
2026-04-06 17:56:54 +0530

    Update code.ino

--- question1/code.ino
+++ question1/code.ino
@@ -9,9 +9,9 @@ unsigned long interval2 = 1000;
 unsigned long interval3 = 1500;
 
 // Store last time each LED changed
-unsigned long previousMillis1 = 0;
-unsigned long previousMillis2 = 0;
-unsigned long previousMillis3 = 0;
+unsigned long previous1 = 0;
+unsigned long previous2 = 0;
+unsigned long previous3 = 0;
 
 // LED states
 bool state1 = LOW;
@@ -25,25 +25,25 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  unsigned long current = millis();
 
   // LED 1
-  if (currentMillis - previousMillis1 >= interval1) {
-    previousMillis1 = currentMillis;
+  if (current - previous1 >= interval1) {
+    previous1 = current;
     state1 = !state1;
     digitalWrite(led1, state1);
   }
 
   // LED 2
-  if (currentMillis - previousMillis2 >= interval2) {
-    previousMillis2 = currentMillis;
+  if (current - previous2 >= interval2) {
+    previous2 = current;
     state2 = !state2;
     digitalWrite(led2, state2);
   }
 
   // LED 3
-  if (currentMillis - previousMillis3 >= interval3) {
-    previousMillis3 = currentMillis;
+  if (current - previous3 >= interval3) {
+    previous3 = current;
     state3 = !state3;
     digitalWrite(led3, state3);
   }
