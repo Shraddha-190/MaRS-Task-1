8cb5452be6c7c548b0cffdb04c92f45b995fb651
Shraddha-190 <ec25i2022@iiitdm.ac.in>
2026-04-06 19:04:31 +0530

    Update code.ino

--- question 2/code.ino
+++ question 2/code.ino
@@ -1,35 +1,36 @@
-int redPin = 11;
-int bluePin = 10;
-int greenPin = 9;
+int red = 11;
+int blue = 10;
+int green = 9;
 
 int ledPin = 5;
 int potPin = A0;
 
-unsigned long previousMillis = 0;
+unsigned long previous = 0;
 int ledState = LOW;
 
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+
   int potValue = analogRead(potPin);
 
-
   int brightness = map(potValue, 0, 1023, 0, 255);
+
   int interval = map(potValue, 0, 1023, 100, 1000);
 
-  
-  analogWrite(redPin, brightness);           // increases
-  analogWrite(greenPin, 255 - brightness);   // decreases
-  analogWrite(bluePin, brightness / 2);      // moderate change
-  unsigned long currentMillis = millis();
+  analogWrite(red, brightness);           // increases
+  analogWrite(green, 255 - brightness);   // decreases
+  analogWrite(blue, brightness / 2);      // moderate change
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  unsigned long current = millis();
+
+  if (current - previous >= interval) {
+    previous = current;
     ledState = !ledState;
     digitalWrite(ledPin, ledState);
   }
