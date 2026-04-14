d94a08909d3d108c57848bb8a8a1985055aba685
Shraddha-190 <ec25i2022@iiitdm.ac.in>
2026-04-14 15:10:25 +0530

    Update code.ino

--- Task 2/code.ino
+++ Task 2/code.ino
@@ -1,21 +1,33 @@
-#include<LiquidCrystal.h>
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 int ir=4;
+int initial=HIGH; //no object initially
 void setup() 
 {
   pinMode(ir,INPUT);
-  lcd.init();
   lcd.begin(0,0);
+  lcd.backlight();
+  Serial.begin(9600);
 }
 
 int count=0;
+bool state;
 void loop() 
 {
 state=digitalRead(ir);
-if(state==LOW)
+if(state==LOW) //object detected
 {
-  count++;
-}
+  if(initial==HIGH) //no object previously
+  {
+    count++;
+    initial=LOW;
+  }
 
+}
+else
+  initial=HIGH;
 lcd.print(count);
+Serial.println(count);
   
 }
